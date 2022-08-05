9f114d994bbc0d8ededae2dc8de1a178dcdf2069
Luis Rodriguez <kike.lerj2710@gmail.com>
2022-08-05 00:14:49 -0400

    se agrego mapa estatico

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,3 +5,16 @@
 // btnDownload.addEventListener('click', ()=>{
 //     console.log('diste click en cv');
 // })
+
+// const iniciarMap = ()=>{
+//     const coord = { lat: 10.0713537 , lng: -69.286727 };
+//     const map = new google.maps.Map(document.querySelector('#map'),{
+//        zoom: 10,
+//        center: coord
+
+//     })
+//     const marker = new google.maps.marker({
+//         position: coord,
+//         map: map
+//     })
+// }
